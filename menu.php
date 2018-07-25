d76e2e02e3237d2ab842680396cae7b7c9161a03
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-07-25 11:28:19 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,8 +1,9 @@
-<ul>
+<?php
+echo "<ul>
   <li><a href="home.php">Home</a></li>
   <li><a href="B&B.php">Bed & Breakfast</a></li>
   <li>Prenotazioni</li>
   <li>Contatti</li>
   <li>Area Personale</li>
-</ul>
-
+</ul>"
+?>
