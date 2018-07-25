b6fb140cff24bfe3c58bce39ac2e4b54d67894f6
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-07-25 14:51:39 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+	<head>
+		<title>BED & BREAKFAST'S's GEG</title>
+	</head>
+	<body>
+		<form action="esegui.php" method="post" name="datiUtenti">
+			Nome : <input type="text" name="nome" /><br/>
+			Età : <input type="text" name="eta" /><br/>
+			Genere : <select name="genere">
+						<option value="uomo" selected="selected">uomo</option>
+						<option value="donna">donna</option>
+					</select><br/>
+			Suggerimenti :</br>
+			<textarea name="testo" cols="40" rows="5"></textarea><br/>
+			<button type="submit">fkjhfduk</button>
+		</form>
+	</body>
+</html>
--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
   <li><a class="home" href="home.php">Home</a></li>
   <li><a class="BandB" href="B&B.php">Bed & Breakfast</a></li>
   <li><a class="pren" href="">Prenotazioni</a></li>
-  <li><a class="contatti" href="">Contatti</a></li>
+  <li><a class="contatti" href="new 1.php">Contatti</a></li>
   <li><a class="AP" href="">Area Personale</a></li>
 </ul>
 
