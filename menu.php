31f8c3fa45e905fe00133dd694b5386de8458e33
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-07-26 15:31:59 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,15 @@
 <ul id="menu">
   <li><a class="home" href="home.php">Home</a></li>
-  <li><a class="BandB" href="B&B.php">Bed & Breakfast</a></li>
+  <li class="dropdown">
+    <a class="BandB" href="B&B.php">Bed & Breakfast</a>
+    <div class="dropwon-content">
+      <a href="stellaviandante.php">B&B La Stella del Viandante</a>
+      <a href="ilBorgo.php">B&B Al Borgo</a>
+      <a href="cadorehaus.php">B&B Cadore Haus</a>
+      <a href="lacorte.php">B&B La Corte</a>
+      <a href="boscoverde.php">B&B Boscoverde</a>
+    </div>
+  </li>
   <li><a class="pren" href="">Prenotazioni</a></li>
   <li><a class="contatti" href="form.php">Contatti</a></li>
   <li><a class="AP" href="">Area Personale</a></li>
