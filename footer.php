f94e86f7ac3a2fe7e659b034c2f627a17614809d
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-07-27 14:07:13 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
-<ul id="footer">
-  <li>Sede Legale: Via Trieste 63, 35121, Padova (IT)</li>
-  <li>Telefono: +39 049 6541498 </li>
-  <li>Fax: +39 049 56214865</li>
-  <li>Email: Bed&BrekfastGeG@info.it</li>
+<div id="footer">
+  <ul>
+    <li>Sede Legale: Via Trieste 63, 35121, Padova (IT)</li>
+    <li>Telefono: +39 049 6541498 </li>
+    <li>Fax: +39 049 56214865</li>
+    <li>Email: Bed&BrekfastGeG@info.it</li>
+    </ul>
   </ul>
-</ul>
+</div>
