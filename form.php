0212fb8ead9e626f3771e0c15bba26e830dac27c
Fgiulia <41376864+Fgiulia@users.noreply.github.com>
2018-07-30 23:47:59 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="fogliocss.css" media="screen"/>
         	<link rel="stylesheet" type="text/css" href="B&B_style.css" media="screen"/>
         	<link rel="stylesheet" type="text/css" href="menu.css" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="form.css" media="screen"/>
 		<link rel="stylesheet" type="text/css" href="footer.css" media="screen"/>
 	</head>
 	<body id="contatti">
@@ -21,19 +22,30 @@
 		<div class="menu">
 			<?php include("menu.php"); ?>
 		</div>
-	
-		<form id="form" action="manda_mail.php" method="post" name="datiUtenti">
-			Nome : <input type="text" name="nome" /><br/>
-			Cognome : <input type="text" name="cognome"/><br/>
-			E-mail : <input type="text" name="email" /><br/>
-			Telefono : <input type="text" name="telefono"/><br/>
-			Messaggio :</br>
-			<textarea name="testo" cols="40" rows="5"></textarea><br/>
-			<input type="checkbox" name="mail" id="action" value="action"/>
-			<label for="Ricevi email">Spunta se vuoi ricevere questo messaggio sulla tua email.</label><br/>
-			<button type="submit">Invia</button>
+		<form id="contact" method="post" action="manda_mail.php" name="datiUtenti">
+			<div>
+				<label class="label-cont" for="nome">Nome</label>
+				<input class="prova" type="text" id="nome" name="nome"><br/>
+
+				<label class="label-cont" for="cognome">Cognome</label>
+				<input class="prova" type="text" id="cognome" name="cognome">
+
+				<label class="label-cont" for="email">E-Mail</label>
+				<input class="prova" type="text" id="email" name="email">
+
+				<label class="label-cont" for="telefono">Telefono</label>
+				<input class="prova" type="text" id="telefono" name="telefono">
+
+				<label class="label-cont" for="message">Messaggio</label>
+				<textarea class="txt" id="message" name="message" placeholder="Scrivi qui la tua richiesta..."></textarea>
+
+				<input type="checkbox" name="mail" id="action" value="action"/>
+				<label for="ricevuta">Contrassegna se vuoi ricevere questo messaggio sulla tua cassella di posta</label><br/>
+
+				<button class="go" type="submit" value="invia">Invia</button>
+			</div>
 		</form>
-		
+
 		<?php include("footer.php"); ?>
 	</body>
 </html>
