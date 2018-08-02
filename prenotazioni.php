75dc02611ce05ed5d2f09dc4da3acd5440391318
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-08-02 16:13:22 +0200

    DA SISTEMARE

--- prenotazioni.php
+++ prenotazioni.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
     <head>
-        <title>Contatti</title>
+        <title>Prenotazioni</title>
         <link rel="stylesheet" type="text/css" href="fogliocss.css" media="screen"/>
         <link rel="stylesheet" type="text/css" href="form.css" media="screen"/>
         <link rel="stylesheet" type="text/css" href="menu.css" media="screen"/>
