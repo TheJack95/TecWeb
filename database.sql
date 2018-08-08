ad5c5738da2079cbb6496ba53a884c61e5afe28d
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2018-08-08 12:00:37 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Prenotazione(
-  ID varchar(10) PRIMARY KEY,
+  ID varchar(10) PRIMARY KEY, -- ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Nome varchar(30) NOT NULL,
   Cognome varchar(30) NOT NULL,
   Telefono varchar(13) DEFAULT NULL,
@@ -7,6 +7,17 @@ CREATE TABLE IF NOT EXISTS Prenotazione(
   Messaggio text NOT NULL,
   Checkin DATE NOT NULL,
   Checkout DATE NOT NULL,
-  BeB  varchar(40) NOT NULL
+  BeB  varchar(40) NOT NULL,
+  Persone int NOT NULL
 );
 
+
+CREATE TABLE IF NOT EXISTS Contatto (
+  ID varchar(10) PRIMARY KEY, -- ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Nome varchar(30) NOT NULL,
+  Cognome varchar(30) NOT NULL,
+  Telefono varchar(13) DEFAULT NULL,
+  Email varchar(50) NOT NULL,
+  Messaggio text NOT NULL,
+);
+-- se id è auto_increment il valore da inserire è NULL
