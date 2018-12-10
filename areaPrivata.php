82696f558e05637a46577b79a228a6cff6d748d2
Giacomo Corrò <giacomocorro@gmail.com>
2018-12-10 13:45:53 +0100

    Correzione errori validazione

--- areaPrivata.php
+++ areaPrivata.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
     <head>
+        <meta charset="utf-8"/>
         <title>Area Personale</title>
         <link rel="stylesheet" type="text/css" href="fogliocss.css" media="screen"/>
         <link rel="stylesheet" media="screen and (max-width:600px)" href="cssMOBILE.css" />
@@ -8,23 +9,24 @@
         <link rel="stylesheet" type="text/css" href="footer.css" media="screen"/>
         <link rel="stylesheet" type="text/css" href="areaPrivata.css" media="screen"/>
     </head>
+
     <body id="AP">
             <div id="header">
                 <div id="logo">
                     <img src="img/logogeg.jpg" alt="logo Bed &amp; Breakfast&rsquo;s GEG"/>
-                    <div id="testologo">
+                    <div class="testologo">
                         Bed &amp; Breakfast
                     </div>
                 </div>
 
                 <div id="logo2">
                     <img src="img/logogeg.jpg" alt="logo Bed &amp; Breakfast&rsquo;s GEG"/>
-                    <div id="testologo">
+                    <div class="testologo">
                         Bed &amp; Breakfast
                     </div>
                 </div>
 
-                <h1  id="titolo"><span xml:lang="en">BED &amp; BREAKFAST&rsquo;S</span> <abbr title="Giulia Erik Giacomo">GEG</abbr></h1>
+                <h1  id="titolo"><span lang="en">BED &amp; BREAKFAST&rsquo;S</span> <abbr title="Giulia Erik Giacomo">GEG</abbr></h1>
 
             </div>
 
