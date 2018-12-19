6019f56aa662f70ab15f4981a478e9e9e89ca402
Fgiulia <fioresegiulia@hotmail.com>
2018-12-19 17:27:11 +0100

    aggiornamento file css

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div id="footer">
     <ul>
-	<li>via Trieste 63</li>
+	    <li>via Trieste 63</li>
         <li>35121 Padova (Italia)</li>
-        <li>Telefono : +39 049 6541498</li>
-        <li>Fax : +39 049 56214865</li>
+        <li>Tel +39 049 6541498</li>
+        <li>Fax +39 049 56214865</li>
         <li>bed&amp;brekfasts<abbr title="Giulia Erik Giacomo">geg</abbr>@info.it</li>
     </ul>
 </div>
