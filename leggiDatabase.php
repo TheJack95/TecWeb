ab30d3a4d529573ce4dc9ea29394f69d527e5a4d
Fgiulia <fioresegiulia@hotmail.com>
2019-01-19 19:14:30 +0100

    aggiornamento file areaPrivata e login

--- leggiDatabase.php
+++ leggiDatabase.php
@@ -12,35 +12,36 @@ $user = "tecwebgeg";
 $password = "4QCvxkNWCZS8";
 $dbname = "my_tecwebgeg";
 
+$username = $_GET["name"];
+
 $conn = mysqli_connect($host, $user, $password, $dbname);
 
 if($username == 'admin') {
     echo "<h2>Prenotazione dei Clienti</h2>";
-    echo "<div id='pren'>";
     $sqlP = "SELECT * FROM Prenotazione ";
     $result = mysqli_query($conn, $sqlP);
     while ($row = mysqli_fetch_assoc($result)) {
-        echo " - Nome: " . $row["Nome"] . "<br>" .
-            " - Cognome: " . $row["Cognome"] . "<br>" .
-            " - Telefono: " . $row["Telefono"] . "<br>" .
-            " - E-mail: " . $row["Email"] . "<br>" .
-            " - Messaggio: " . $row["Messaggio"] . "<br>" .
-            " - Check-in: " . $row["Checkin"] . "<br>" .
-            " - Check-out: " . $row["Checkout"] . "<br>" .
-            " - Struttura: " . $row["BeB"] . "<br>" .
-            " - N. Persone: " . $row["Persone"] . "<br>" .
-            "<br>" . "</div>";
+        echo "<div class='pren'><table><tr><td>" . "Nome</td><td>" . $row["Nome"] . "</td></tr>" .
+            "<tr><td>Cognome</td><td> " . $row["Cognome"] . "</td></tr>" .
+            "<tr><td>Telefono</td><td> " . $row["Telefono"] . "</td></tr>" .
+            "<tr><td>E-mail</td><td> " . $row["Email"] . "</td></tr>" .
+            "<tr><td>Messaggio</td><td> " . $row["Messaggio"] . "</td></tr>" .
+            "<tr><td>Check-in</td><td> " . $row["Checkin"] . "</td></tr>" .
+            "<tr><td>Check-out</td><td> " . $row["Checkout"] . "</td></tr>" .
+            "<tr><td>Struttura</td><td> " . $row["BeB"] . "</td></tr>" .
+            "<tr><td>N. Persone</td><td> " . $row["Persone"] . "</td></tr>" .
+            "</td></tr>" . "</table></div>";
     }
     echo "<h2>Messaggi dai Clienti</h2>";
     $sqlC = "SELECT * FROM Contatto ";
     $result = mysqli_query($conn, $sqlC);
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<div id='pren'>" . " - Nome: " . $row["Nome"] . "<br>" .
-            " - Cognome: " . $row["Cognome"] . "<br>" .
-            " - Telefono: " . $row["Telefono"] . "<br>" .
-            " - E-mail: " . $row["Email"] . "<br>" .
-            " - Messaggio: " . $row["Messaggio"] . "<br>" .
-            "<br>" . "</div>";
+        echo "<div class='pren'><table><tr><td>" . "Nome</td><td> " . $row["Nome"] . "</td></tr>" .
+            "<tr><td>Cognome</td><td> " . $row["Cognome"] . "</td></tr>" .
+            "<tr><td>Telefono</td><td> " . $row["Telefono"] . "</td></tr>" .
+            "<tr><td>E-mail</td><td> " . $row["Email"] . "</td></tr>" .
+            "<tr><td>Messaggio</td><td> " . $row["Messaggio"] . "</td></tr>" .
+            "</td></tr>" . "</table></div>";
     }
 }
 else {
@@ -48,20 +49,18 @@ else {
     $sqlP = "SELECT * FROM Prenotazione WHERE Username = '$username' ";
     $result = mysqli_query($conn, $sqlP);
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<div id='pren'>" . " - Nome: " . $row["Nome"] . "<br>" .
-            " - Cognome: " . $row["Cognome"]."<br>" .
-            " - Telefono: " . $row["Telefono"]."<br>" .
-            " - E-mail: " . $row["Email"]."<br>" .
-            " - Messaggio: " . $row["Messaggio"]."<br>" .
-            " - Check-in: " . $row["Checkin"]."<br>" .
-            " - Check-out: " . $row["Checkout"]."<br>" .
-            " - Struttura: " . $row["BeB"]."<br>" .
-            " - N. Persone: " . $row["Persone"]."<br>" .
-            "<br>";
+        echo "<div class='pren'><table><tr><td>" . "Nome</td><td> " . $row["Nome"] . "</td></tr>" .
+            "<tr><td>Cognome</td><td> " . $row["Cognome"]."</td></td>" .
+            "<tr><td>Telefono</td><td> " . $row["Telefono"]."</td></tr>" .
+            "<tr><td>E-mail</td><td> " . $row["Email"]."</td></tr>" .
+            "<tr><td>Messaggio</td><td> " . $row["Messaggio"]."</td></tr>" .
+            "<tr><td>Check-in</td><td> " . $row["Checkin"]."</td></tr>" .
+            "<tr><td>Check-out</td><td> " . $row["Checkout"]."</td></tr>" .
+            "<tr><td>Struttura</td><td> " . $row["BeB"]."</td></tr>" .
+            "<tr><td>N. Persone</td><td> " . $row["Persone"]."</td></tr>" .
+            "</td></tr></table></div>";
     }
-    echo "<form action=\"prenotazioni.php\" >
-        <button class='btnP' type=\"submit\">Effettua una prenotazione</button>
-        </form>";
+    echo "<div class=\"prenotazionebutton\"><a href=\"prenotazioni.php\" class='btnP'>Effettua una prenotazione</a></div>";
     echo "</div>";
 }
 $conn->close();
--- loginvalid.php
+++ loginvalid.php
@@ -4,14 +4,14 @@ error_reporting(0);
 
 $val_user = $val_pass = "";
 $user = $pass = "";
-$booluser = $boolpass = $login_ok = false;
+$booluser = $boolpass = false;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty( trim($_POST["user"]) )) {
         $val_user = "CAMPO OBBLIGATORIO VUOTO\n";
         $login_ok = false;
     } else {
-        $user = test_input($_POST["user"]);
+        $user = $_POST["user"];
         $booluser = true;
     }
 
@@ -19,30 +19,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $val_pass = "CAMPO OBBLIGATORIO VUOTO\n";
         $login_ok = false;
     } else {
-        $pass = test_input($_POST["pass"]);
+        $pass = $_POST["pass"];
         $boolpass = true;
     }
 
-    if($booluser && $boolpass)
-        $login_ok = true;
-}
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-/*if($login_ok === true) {
+    if($booluser && $boolpass) {
+    /*
+    //credenziali unipd
     $host = "localhost";
-    $user = "gfiorese";
-    $pass = "eeNgeeghaiBooqu1";
+    $username = "gfiorese";
+    $password = "eeNgeeghaiBooqu1";
     $dbname = "gfiorese";
+    */
 
-    $connessione = mysqli_connect($host, $user, $pass, $dbname);
+    //credenziali altervista
+    $host = "localhost";
+    $username = "tecwebgeg";
+    $password = "4QCvxkNWCZS8";
+    $dbname = "my_tecwebgeg";
+
+    $connessione = mysqli_connect($host, $username, $password, $dbname);
+    $checkuser = "SELECT * FROM Utente WHERE Login = '".$user."' && Password = '".$pass."'";
+
+    $result = mysqli_query($connessione, $checkuser);
+
+    if(mysqli_num_rows($result)==0)
+        echo "<div class='error_message'>USERNAME E PASSWORD NON VALIDI</div>";
+    else {
+            header("location: /areaPrivata.php?name=$user");
+    }
 
     $connessione->close();
-}*/
-
+}
+}
 ?>
+
