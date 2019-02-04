729932b6c50ecdaa3e05ddf51a3496024abce356
TheJack95 <41376398+TheJack95@users.noreply.github.com>
2019-02-04 15:01:02 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -34,16 +34,22 @@
         <?php include("menu.php"); ?>
 
         <div class="tutto">
+            <h1 class="titolo">Chi siamo</h1>
             <p class="testo">
-                <img id="homerosolina" src="img/mare.png" alt="foto del mare" />
                 <span xml:lang="en">Bed &amp; Breakfast&rsquo;s</span>  <abbr title="Giulia Erik Giacomo">G.E.G.</abbr> nasce nel 2017
                 dall&rsquo;idea di tre giovani imprenditori con l&rsquo;intenzione di valorizzare delle strutture gi&agrave; esistenti, ma
-                troppo piccole per essere competitive singolarmente, creando una catena, senza per&ograve; uniformarle. L&rsquo;idea &egrave;
-                quella di permettere ai viaggiatori di conoscere le bellezze del luogo in cui si trovano e ci&ograve; &egrave; possibile
-                solamente grazie al nostro impegno costante per la manutenzione delle caratteristiche delle singole strutture.
+                troppo piccole per essere competitive singolarmente, creando una catena, senza per&ograve; uniformarle.
+                L&rsquo;idea &egrave; quella di permettere ai viaggiatori di conoscere le bellezze del luogo in cui si trovano e ci&ograve; &egrave; possibile
+                solamente grazie al nostro impegno costante per la manutenzione delle caratteristiche delle singole strutture.</p>
+            <img id="homerosolina" src="img/mare.png" alt="foto del mare" />
+            <h1 class="sTitolo">Dove siamo</h1>
+            <p class="testo">
                 Le localit&agrave; dove sono collocati i <abbr title="Bed and Breakfast" xml:lang="en">B&B</abbr> sono molto diverse tra loro:
                 Falcade, Nevegal e Auronzo di Cadore (tutte situate in provincia Belluno) si trovano immerse nelle stupende Dolomiti Venete,
-                mentre le strutture di Arqu&agrave; Petrarca (PD) e Rosolina (RO) si trovano nella Pianura Padana Veneta. Le nostre strutture 			dispongono tutte di ampie camere con due o quattro posti letto, bagno privato, un piccolo angolo cottura e un tavolo da pranzo.
+                mentre le strutture di Arqu&agrave; Petrarca (PD) e Rosolina (RO) si trovano nella Pianura Padana Veneta. Le nostre strutture
+                dispongono tutte di ampie camere con due o quattro posti letto, bagno privato, un piccolo angolo cottura e un tavolo da pranzo.</p>
+            <h1 class="sTitolo">Prezzi</h1>
+            <p class="testo">
                 Le camere sono affittabili da &euro; 15 a &euro; 30 a persona a notte nel periodo di bassa stagione e da &euro; 20 a &euro; 40
                 a persona a notte nel periodo di alta stagione, mentre la tassa varia in base alla localit&agrave;.</br>
                 Se vuoi informazioni pi&ugrave; precise <a class="contatti" href="contatti.php">contattaci</a>, siamo a
@@ -59,7 +65,7 @@
                     <li>Possibilit&agrave; di ricarica per <span xml:lang="en">E&ndash;bike</span></li>
                     <li>Connessione <span xml:lang="en">Wi&ndash;Fi</span> nelle camere</li>
                 </ul>
-                <ul class="lista2">
+                <ul class="lista">
                     <li><strong>I servizi disponibili a pagamento:</strong></li>
                     <li>Pranzo al sacco </li>
                     <li>Noleggio biciclette</li>
